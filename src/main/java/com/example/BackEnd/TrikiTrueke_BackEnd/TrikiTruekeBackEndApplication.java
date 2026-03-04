5ffacc45c9a17b7bffda6ff3895c7c94bb3636a5
HervinCajas <hervindc15@gmail.com>
2026-03-04 16:04:10 -0500

    Inicializacion del proyecto

---
+++ src/main/java/com/example/BackEnd/TrikiTrueke_BackEnd/TrikiTruekeBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.BackEnd.TrikiTrueke_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrikiTruekeBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrikiTruekeBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BackEnd/TrikiTrueke_BackEnd/TrikiTruekeBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BackEnd.TrikiTrueke_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrikiTruekeBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
