1dedb8fede14f06fc14ecdaae20bbe5ff95355d2
Carlos Andres Reyes Fernandez <andres22.repositori@gmail.com>
2026-03-11 16:26:52 -0500

    CategoriaDTO

--- src/main/java/com/example/BackEnd/TrikiTrueke_BackEnd/Model/CategoriaDTO.java
+++ src/main/java/com/example/BackEnd/TrikiTrueke_BackEnd/Model/CategoriaDTO.java
@@ -3,14 +3,45 @@ package com.example.BackEnd.TrikiTrueke_BackEnd.Model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.Date;
-import java.util.List;
-
 @Document(collection = "Categorias")
 public class CategoriaDTO {
 
     @Id
     private String id;
-    private String titulo;
+    private String nombre;
+    private String descripcion;
 
+    public CategoriaDTO() {
+    }
+
+    public CategoriaDTO(String id, String nombre, String descripcion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
